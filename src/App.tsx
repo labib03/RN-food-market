bd5253fe2e1c1e254128e617578f7864c1a6ff1e
l_asrdn <ansorudinlabib1@gmail.com>
2023-03-30 17:20:02 +0700

    feat: setup react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {SplashScreen} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <NavigationContainer>
       <SplashScreen />
-    </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
